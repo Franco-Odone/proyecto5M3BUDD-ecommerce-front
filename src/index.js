bfcb2fcd22d5cabcfba84baa53a16e18dafb0ea5
Franco Odone <108704414+Franco-Odone@users.noreply.github.com>
2023-01-17 22:51:01 -0300

    inicio pruebas implementacion redux

--- src/index.js
+++ src/index.js
@@ -2,18 +2,35 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+// Provider para manejar la data de los productos
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+
+// Se cambia el nombre ya que el default export es en realidad un reducer
+import productsReducer, { productsFetch } from "./features/productsSlice";
+
+import { ProductContextProvider } from "./components/context/ProductContext";
+
 import App from "./App/App";
 import "./index.css";
 
-import { ProductContextProvider } from "./components/context/ProductContext";
+const store = configureStore({
+  reducer: {
+    products: productsReducer,
+  },
+});
+
+store.dispatch(productsFetch());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ProductContextProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ProductContextProvider>
+    <Provider store={store}>
+      <ProductContextProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ProductContextProvider>
+    </Provider>
   </React.StrictMode>
 );
